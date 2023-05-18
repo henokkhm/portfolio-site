a549aaab766cdcc05dc51f831595910d16d08237
Henok Kirubel <henokhm2@gmail.com>
2023-05-18 11:14:41 +0300

    Add project modal scripts

---
+++ src/js/project-details-modal.js
@@ -0,0 +1,16 @@
+// Import the data
+// Import the generateModalHTML helper
+
+// Get the parent element of all the project cards
+
+// Add an event listner that uses event delegation to check if a
+// "See project" button was clicked
+
+// Use the data-id attribute to get the id of the project that should
+// be shown in the modal
+
+// Generate the HTML of that modal and add it to the page (using the imports above)
+
+// Get the modal-wrapper and the close button
+// For each of them, add an event listner that checks whether they were clicked
+// if they get clicked, close the modal and remove its content from the HTML
