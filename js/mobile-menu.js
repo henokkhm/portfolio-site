5225569ae047f6db67d94d0138e3c7c7d283adce
Henok Kirubel <henokhm2@gmail.com>
2023-06-03 12:33:38 +0300

    Introduced bugs

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,9 @@
 const moblieMenuToggle = document.querySelector('#mobile-menu-toggle');
 const mobileNav = document.querySelector('#mobile-nav');
-const mobileNavLink = document.querySelectorAll('.header__nav__mobile__link');
 
 const handleOpenMobileNav = () => {
   mobileNav.setAttribute('data-visible', true);
   moblieMenuToggle.setAttribute('aria-expanded', true);
-  moblieMenuToggle.setAttribute('src', 'assets/icons/close-menu.svg');
   moblieMenuToggle.setAttribute('alt', 'Close mobile navigation icon');
   document.body.style.overflow = 'hidden';
 };
@@ -13,7 +11,6 @@ const handleOpenMobileNav = () => {
 const handleCloseMobileNav = () => {
   mobileNav.setAttribute('data-visible', false);
   moblieMenuToggle.setAttribute('aria-expanded', false);
-  moblieMenuToggle.setAttribute('src', 'assets/icons/hamburger.svg');
   moblieMenuToggle.setAttribute('alt', 'Open mobile navigation icon');
   document.body.style.overflow = 'auto';
 };
@@ -27,11 +24,6 @@ moblieMenuToggle.addEventListener('click', () => {
   if (visiblity === 'true') {
     handleCloseMobileNav();
   }
-});
-
-// Handle clicking a mobile menu link
-mobileNavLink.forEach((link) => {
-  link.addEventListener('click', handleCloseMobileNav);
 });
 
 // Hide mobile navbar and blurred div
